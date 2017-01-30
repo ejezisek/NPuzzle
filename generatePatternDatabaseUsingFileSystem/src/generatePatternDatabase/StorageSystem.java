606ea5b2e98349e5c385851c4bcd9593b0fde5c1
Edward Jezisek <ejezisek@gmail.com>
2017-01-30 00:54:31 -0500

    Removed unneeded project items.

--- generatePatternDatabaseUsingFileSystem/src/generatePatternDatabase/StorageSystem.java
+++
@@ -1,6 +0,0 @@
-package generatePatternDatabase;
-
-public interface StorageSystem {
-	public void addMultiple(String[] location, String []value, int[] numToDestination);
-public boolean[] exists(String[] location, String[] value);
-}
